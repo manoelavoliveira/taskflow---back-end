dea8d316205f5ffa5d3bad8471f4eb697c37da3f
Manoela Vitoria Lemos de Oliveira <manoela.v.oliveira@aluno.senai.br>
2026-08-24 11:17:36 -0300

    24/08

---
+++ globals.js
@@ -0,0 +1,11 @@
+console.log(__dirname);
+
+console.log(__filename);
+
+console.log(process.version);
+
+console.log(process.platform);
+
+console.log(process.env.PATH);
+
+console.log(process.argv);
--- ola.js
+++
@@ -1,3 +0,0 @@
-console.log('Ola, Node.js!');
-
-const nome = 'TaskFlow API'
