32d4b58ed64a8c7feec46f59fa066c2b468ed16b
Manoela Vitoria Lemos de Oliveira <manoela.v.oliveira@aluno.senai.br>
2026-09-02 10:46:45 -0300

    MVC - controllers

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,86 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const usuariosController = require("../controllers/usuarios.controller");
 
-let usuarios = [
-  { id: 1, nome: "admin", email: "admin@gmail.com", senha: "1234" },
-  { id: 2, nome: "Ana", email: "Ana@hotmail.com", senha: "ana567" },
-  { id: 3, nome: "João", email: "João@outlook.com", senha: "joao890" },
-  { id: 4, nome: "Maria", email: "Maria@uol.com", senha: "maria00 " },
-];
-let proximoIdUsuario = 5;
+router.get("/", usuariosController.listar);
 
-router.get("/", (req, res) => {
-  const { nome } = req.query;
-  let resultado = usuarios;
+router.get("/:id", usuariosController.buscarPorId)
 
-  if (nome) {
-    resultado = resultado.filter((u) => u.nome === nome);
-  }
+router.post("/", usuariosController.criar)
 
-  res.json(resultado);
-});
+router.put("/:id", usuariosController.atualizar);
 
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const usuario = usuarios.find((u) => u.id === id);
-
-  if (!usuario) {
-    return res.status(404).json({ erro: "Usuário não encontrado" });
-  }
-
-  res.json(usuario);
-});
-
-router.post("/", (req, res) => {
-  const { nome, email, senha } = req.body;
-  const emailExiste = usuarios.find((u) => u.email === email);
-
-  if (!nome || !email) return res.status(400).json({ erro: "Nome e email obrigatórios!" });
-
-  if (emailExiste) {
-    return res.status(400).json({
-      erro: "Este email já está cadastrado",
-    });
-  }
-  const novoUsuario = {
-    id: proximoIdUsuario++,
-    nome: nome || "",
-    email: email || "",
-    senha: senha || "",
-  };
-
-  usuarios.push(novoUsuario);
-  res.status(201).json(novoUsuario);
-});
-
-router.put("/:id", (req, res) => {
-   const id = parseInt(req.params.id);
-   const {nome, email, senha } = req.body;
-   const indice = usuarios.findIndex((u) => u.id === id);
-   const emailExiste = usuarios.find((u) => u.email === req.body.email && u.id !== id);
-
-   if (emailExiste) {
-     return res.status(400).json({ erro: "Este email já está cadastrado" });
-   }
-   if (indice === -1) {
-     return res.status(404).json({ erro: "Usuário não encontrado" });
-   }
-
-   const usuarioAtualizado = { id, nome, email, senha };
-   usuarios[indice] = usuarioAtualizado;
-
-   res.json(usuarioAtualizado);
- });
-
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const indice = usuarios.findIndex((u) => u.id === id);
-
-  if (indice === -1)
-    return res.status(404).json({ erro: "Tarefa não encontrada" });
-
-  const removido = usuarios.splice(indice, 1)[0];
-  res.json({ mensagem: "Usuário removido com sucesso", id });
-});
+router.delete("/:id", usuariosController.remover);
 
 module.exports = router;
